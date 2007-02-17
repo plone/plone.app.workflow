f8997ec141368b5b7d0cb3334041a11df3e06e23
Martin Aspeli <optilude@gmail.com>
2007-02-17 01:24:51 +0000

    It's very hard to sit in a hotel room with Limi without being told to fix some UI things.
    svn path=/plone.app.workflow/trunk/; revision=12496

--- plone/app/workflow/localroles.py
+++ plone/app/workflow/localroles.py
@@ -1,42 +1,44 @@
 from zope.interface import implements
 from plone.app.workflow.interfaces import ISharingPageRole
 
+from Products.CMFPlone import PloneMessageFactory as _
+
 # These are for everyone
 
 class ReaderRole(object):
     implements(ISharingPageRole)
     
-    title = u"View"
+    title = _(u"Can view")
     required_permission = None
     
 class EditorRole(object):
     implements(ISharingPageRole)
     
-    title = u"Edit"
+    title = _(u"Can edit")
     required_permission = None
     
 class ReviewerRole(object):
     implements(ISharingPageRole)
     
-    title = u"Review"
+    title = _(u"Can review")
     required_permission = None
     
 # Only managers can manage these
     
-class ManagerRole(object):
-    implements(ISharingPageRole)
-    
-    title = u"Manage"
-    required_permission = 'Manage portal'
-    
-class OwnerRole(object):
-    implements(ISharingPageRole)
-    
-    title = u"Own"
-    required_permission = 'Manage portal'
-    
-class MemberRole(object):
-    implements(ISharingPageRole)
-    
-    title = u"Member"
-    required_permission = 'Manage portal'
+# class ManagerRole(object):
+#     implements(ISharingPageRole)
+#     
+#     title = u"Manage"
+#     required_permission = 'Manage portal'
+#     
+# class OwnerRole(object):
+#     implements(ISharingPageRole)
+#     
+#     title = u"Own"
+#     required_permission = 'Manage portal'
+#     
+# class MemberRole(object):
+#     implements(ISharingPageRole)
+#     
+#     title = u"Member"
+#     required_permission = 'Manage portal'
