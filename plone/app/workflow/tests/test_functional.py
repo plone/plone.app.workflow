5e05872a8648436b138dfb01e2b2ae33556c29b0
Andrew Burkhalter <andrewburkhalter@gmail.com>
2007-03-04 18:30:02 +0000

    updating our FunctionalTestClass in base with a few helper methods that will be useful in functional tests, adding test_functional test_suite which will run all the *.txt ftest, and adding a first batch of functional tests for the onestateworkflow
    svn path=/plone.app.workflow/trunk/; revision=13457

---
+++ plone/app/workflow/tests/test_functional.py
@@ -0,0 +1,15 @@
+from zope.testing import doctest
+from unittest import TestSuite
+from utils import optionflags
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from base import WorkflowFunctionalTestCase
+
+def test_suite():
+    tests = ['OneStateWorkflow.txt',]
+    suite = TestSuite()
+    for test in tests:
+        suite.addTest(FunctionalDocFileSuite(test,
+            optionflags=optionflags,
+            package="plone.app.workflow.tests",
+            test_class=WorkflowFunctionalTestCase))
+    return suite
