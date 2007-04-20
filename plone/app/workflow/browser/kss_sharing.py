2b8b557c7afd168a10fc56a95075f5f9d14eae41
Martin Aspeli <optilude@gmail.com>
2007-04-20 22:49:41 +0000

    This doesn't properly work, but committing - we need a fix/feature in KSS to handle submitting forms alongside AJAX request when they have :records elements. Working on that.
    svn path=/plone.app.workflow/trunk/; revision=14405

--- plone/app/workflow/browser/kss_sharing.py
+++ plone/app/workflow/browser/kss_sharing.py
@@ -14,20 +14,38 @@ class KssSharingView(base):
 
     template = ViewPageTemplateFile('sharing.pt')
     macro_wrapper = ViewPageTemplateFile('macro_wrapper.pt')
+    
+    def updateSharingInfo(self, search_term='', form_vars={}):
+    
+        sharing = getMultiAdapter((self.context, self.request,), name="sharing")
+    
+        inherit = bool(form_vars.get('inherit', False))
+        sharing.update_inherit(inherit)
+        
+        # XXX: This doesn't work because the request marshalling appears to
+        # be wrong, especially when there are multiple rows with things 
+        # selected.
+        entries = form_vars.get('entries', [])
+        
+        roles = [r['id'] for r in sharing.roles()]
+        settings = []
+        for entry in entries:
+            settings.append(
+                dict(id = entry['id'],
+                     type = entry['type'],
+                     roles = [r for r in roles if entry.get('role_%s' % r, False)]))
+        if settings:
+            sharing.update_role_settings(settings)
 
-    
-    def updateSharingInfo(self, search_term=''):
-    
         # get the table body, let it render again
         # use macro in sharing.pt for that
-
-        sharing = getMultiAdapter((self.context, self.request,), name="sharing")
 
         # get the html from a macro
         the_id = 'user-group-sharing-settings'
         ksscore = self.getCommandSet('core')
         macro = self.template.macros[the_id]
         res = self.macro_wrapper(the_macro=macro, instance=self.context, view=sharing)
+        
         # self.macroContent does not work, it used restrictedTraverse
         ksscore.replaceHTML(ksscore.getHtmlIdSelector(the_id), res)
         the_id = 'user-group-sharing-head'
