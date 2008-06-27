2d8eda7c6706e3192e8ad3e19d2d06248b0a4d88
Wichert Akkerman <wichert@wiggy.net>
2008-06-27 09:11:17 +0000

    Properly register the permissions with Zope.
    Mark the security names as public so they can be imported everywhere
    and register them with Zope on startup so you can manage them via the
    ZMI or a GenericSetup profile. refs #7652.
    
    
    svn path=/plone.app.workflow/trunk/; revision=21290

--- plone/app/workflow/__init__.py
+++ plone/app/workflow/__init__.py
@@ -1,1 +1,2 @@
-#
+# Register the permissions with Zope
+import plone.app.workflow.permissions
--- plone/app/workflow/permissions.py
+++ plone/app/workflow/permissions.py
@@ -1,16 +1,28 @@
 from Products.CMFCore.permissions import setDefaultRoles
+from AccessControl import ModuleSecurityInfo
 
-DelegateRoles = "Sharing page: Delegate roles"
-DelegateReaderRole = "Sharing page: Delegate Reader role"
-DelegateEditorRole = "Sharing page: Delegate Editor role"
-DelegateContributorRole = "Sharing page: Delegate Contributor role"
-DelegateReviewerRole = "Sharing page: Delegate Reviewer role"
+security = ModuleSecurityInfo("plone.app.workflow.permissions")
 
 # Controls access to the "sharing" page
+security.declarePublic("DelegateRoles")
+DelegateRoles = "Sharing page: Delegate roles"
 setDefaultRoles(DelegateRoles, ('Manager', 'Owner', 'Editor', 'Reviewer', ))
 
 # Control the individual roles
+security.declarePublic("DelegateReaderRole")
+DelegateReaderRole = "Sharing page: Delegate Reader role"
 setDefaultRoles(DelegateReaderRole, ('Manager', 'Owner', 'Editor', 'Reviewer'))
+
+security.declarePublic("DelegateEditorRole")
+DelegateEditorRole = "Sharing page: Delegate Editor role"
 setDefaultRoles(DelegateEditorRole, ('Manager', 'Owner', 'Editor'))
+
+security.declarePublic("DelegateContributorRole")
+DelegateContributorRole = "Sharing page: Delegate Contributor role"
 setDefaultRoles(DelegateContributorRole, ('Manager', 'Owner',))
+
+security.declarePublic("DelegateReviewerRole")
+DelegateReviewerRole = "Sharing page: Delegate Reviewer role"
 setDefaultRoles(DelegateReviewerRole, ('Manager', 'Reviewer',))
+
+
