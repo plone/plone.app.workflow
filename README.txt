207860d672fcea10115e7aea95899a3be49a2190
Martin Aspeli <optilude@gmail.com>
2009-08-09 13:31:01 +0000

    Implement PLIP refs #9263.
    svn path=/plone.app.workflow/branches/plip9263-sharing-gs/; revision=28559

--- README.txt
+++ README.txt
@@ -4,3 +4,14 @@ Introduction
 plone.app.workflow contains workflow- and security-related features for Plone,
 including the sharing view.
 
+It also supports the `sharing.xml` GenericSetup syntax, to add new roles to
+the "Sharing" page.::
+
+    <sharing xmlns:i18n="http://xml.zope.org/namespaces/i18n" i18n:domain="plone">
+        <role
+            id="CopyEditor"
+            title="Can edit copy"
+            permission="Manage portal"
+            i18n:attributes="title"
+            />
+    </sharing>
---
+++ plone/app/workflow/exportimport.py
@@ -0,0 +1,121 @@
+from persistent import Persistent
+from zope.interface import implements
+
+from plone.app.workflow.interfaces import ISharingPageRole
+
+from zope.component import adapts
+from zope.component import getSiteManager
+from zope.component import queryMultiAdapter
+from zope.component.interfaces import IComponentRegistry
+
+from Products.GenericSetup.interfaces import IBody
+from Products.GenericSetup.interfaces import ISetupEnviron
+from Products.GenericSetup.utils import XMLAdapterBase
+
+from zope.i18nmessageid import MessageFactory
+PMF = MessageFactory('plone')
+
+class PersistentSharingPageRole(Persistent):
+    """These are registered as local utilities when managing the sharing
+    page roles.
+    """
+    implements(ISharingPageRole)
+    
+    title = u""
+    required_permission = None
+    
+    def __init__(self, title=u"", required_permission=None):
+        self.title = PMF(title)
+        self.required_permission = required_permission
+
+
+class SharingXMLAdapter(XMLAdapterBase):
+    adapts(IComponentRegistry, ISetupEnviron)
+
+    _LOGGER_ID = 'plone.app.workflow'
+
+    name = 'plone.app.workflow.sharing'
+    info_tag = u"__sharing_gs__"
+
+    def _importNode(self, node):
+        
+        if self.environ.shouldPurge():
+            self._purgeRoles()
+
+        for child in node.childNodes:
+            self._initRole(child)
+
+    def _exportNode(self):
+        node = self._doc.createElement('sharing')
+        
+        for reg in self._iterRoleRegistrations():
+            node.appendChild(self._extractRole(reg))
+
+        return node
+
+    def _iterRoleRegistrations(self):
+        for reg in tuple(self.context.registeredUtilities()):
+            if reg.provided.isOrExtends(ISharingPageRole) \
+                    and isinstance(reg.info, basestring)  \
+                    and self.info_tag in reg.info:
+                yield reg
+
+    def _purgeRoles(self):
+        for reg in self._iterRoleRegistrations():
+            self.context.unregisterUtility(provided=reg.provided, name=reg.name)
+
+    def _initRole(self, node):
+        
+        if node.nodeName != 'role':
+            return
+            
+        name = unicode(node.getAttribute('id'))
+        title = unicode(node.getAttribute('title'))
+        required = node.getAttribute('permission') or None
+
+        component = PersistentSharingPageRole(title=title, required_permission=required)
+
+        self.context.registerUtility(component, ISharingPageRole, name, info=self.info_tag)
+
+    def _extractRole(self, reg):
+        
+        component = reg.component
+        
+        node = self._doc.createElement('role')
+        node.setAttribute('id', reg.name)
+        node.setAttribute('title', component.title)
+        
+        if component.required_permission:
+            node.setAttribute('permission', component.required_permission)
+        
+        return node
+
+def import_sharing(context):
+    
+    sm = getSiteManager(context.getSite())
+    logger = context.getLogger('plone.app.workflow')
+    
+    if sm is None or not IComponentRegistry.providedBy(sm):
+        logger.info("Can not register sharing page roles, as no component registry was found.")
+        return
+
+    importer = queryMultiAdapter((sm, context), IBody, name=u"plone.app.workflow.sharing")
+    if importer:
+        body = context.readDataFile('sharing.xml')
+        if body is not None:
+            importer.body = body
+    
+def export_sharing(context):
+
+    sm = getSiteManager(context.getSite())
+    logger = context.getLogger('plone.app.workflow')
+    
+    if sm is None or not IComponentRegistry.providedBy(sm):
+        logger.debug("Nothing to export.")
+        return
+
+    exporter = queryMultiAdapter((sm, context), IBody, name=u"plone.app.workflow.sharing")
+    if exporter:
+        body = exporter.body
+        if body is not None:
+            context.writeDataFile('sharing.xml', body, exporter.mime_type)
