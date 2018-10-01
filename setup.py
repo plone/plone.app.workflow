5644490f1b560756b537e46217b172ecf21fa18a
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:37:03 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     keywords='workflow sharing plone',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.app.workflow',
+    url='https://pypi.org/project/plone.app.workflow',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
