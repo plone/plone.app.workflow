534cd49ba595e8ba936905f858ff0f4943e0b50d
Maurits van Rees <maurits@vanrees.org>
2021-09-15 16:03:17 +0200

    Added Python 3.9 classifier.
    [ci skip]

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords='workflow sharing plone',
     author='Plone Foundation',
